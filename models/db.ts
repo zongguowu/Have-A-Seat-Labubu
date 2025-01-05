cd7db1a4e44a68b14bc4d2884a7359761e4f718c
idoubi <me@idoubi.cc>
2025-01-05 12:34:47 +0800

    update supabase key

--- models/db.ts
+++ models/db.ts
@@ -1,10 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function getSupabaseClient() {
-  const client = createClient(
-    process.env.SUPABASE_URL || "",
-    process.env.SUPABASE_ANON_KEY || ""
-  );
+  const supabaseUrl = process.env.SUPABASE_URL || "";
+
+  let supabaseKey = process.env.SUPABASE_ANON_KEY || "";
+  if (process.env.SUPABASE_SERVICE_ROLE_KEY) {
+    supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+  }
+
+  const client = createClient(supabaseUrl, supabaseKey);
 
   return client;
 }
